0f81ab6d874c6f740f144a991bfcec60f3dfa088
zhangyemengren <624593758@qq.com>
2024-08-19 09:07:17 +0800

    update

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,10 @@
+use std::sync::{LazyLock, Mutex};
+
+static TOKEN: LazyLock<Mutex<String>> = LazyLock::new(|| Mutex::new("".to_string()));
+
 fn main(){
-    println!("Hello, world!");
+    let mut token = TOKEN.lock().unwrap();
+    println!("Hello, world! {}", token);
+    *token = "1234".to_string();
+    println!("token will be {}", token);
 }
