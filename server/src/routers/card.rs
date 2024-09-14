8609ef962fa283872fe3f10df22405435621ccd0
zhangyemengren <624593758@qq.com>
2024-09-14 16:13:21 +0800

    update

--- server/src/routers/card.rs
+++ server/src/routers/card.rs
@@ -21,19 +21,25 @@ fn default_page() -> i32 {
     1
 }
 
+fn default_type() -> String {
+    "minion".to_string()
+}
+
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct PageQuery {
     #[serde(default = "default_locale")]
     pub locale: String,
-    #[serde(default)]
-    pub class: String,
     #[serde(default = "default_set")]
     pub set: String,
+    pub class: String,
     #[serde(default = "default_page_size")]
     pub page_size: i32,
     #[serde(default = "default_page")]
     pub page: i32,
+    #[serde(default = "default_type", rename = "type")]
+    pub s_type: String,
+    pub mana_cost: String,
 }
 
 pub async fn get_cards(
@@ -48,11 +54,15 @@ pub async fn get_cards(
         set,
         page_size,
         page,
+        s_type,
+        mana_cost
     } = params;
     let url = format!(
         "https://us.api.blizzard.com/hearthstone/cards/?\
-        class={class}&\
         set={set}&\
+        class={class}&\
+        manaCost={mana_cost}&\
+        type={s_type}&\
         pageSize={page_size}&\
         page={page}&\
         sort=manaCost:asc,name:asc,classes:asc,groupByClass:asc,groupByClass:asc&\
